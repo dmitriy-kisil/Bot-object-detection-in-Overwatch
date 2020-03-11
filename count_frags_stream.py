09d324a1deb00d8309ada096084aa4949036120c
Dmitriy Kisil <logart1995@gmail.com>
2020-03-11 23:08:08 +0200

    Refactor + add numpy in requirements.txt

--- count_frags_stream.py
+++ count_frags_stream.py
@@ -3,15 +3,13 @@ import cv2
 import time
 import mss
 
-
 frame_width = 1280
 frame_height = 720
-frame_rate = 10.0
+frame_rate = 15.0
 OUTPUT_FILE = "output4.avi"
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter(OUTPUT_FILE, fourcc, frame_rate,
                       (frame_width, frame_height))
-
 with mss.mss() as sct:
     # Part of the screen to capture
     monitor = {"top": 120, "left": 280, "width": 1368, "height": 770}
@@ -19,82 +17,83 @@ with mss.mss() as sct:
     total_frags = 0
     number_of_frags = 0
     while True:
+        # Time which frame has been captured, need to show how many seconds screen is recorded
         last_time = time.time()
-
         # Get raw pixels from the screen, save it to a Numpy array
         img = np.array(sct.grab(monitor))
+        # Resize to better performance
         img = cv2.resize(img, (1280, 720))
-
+        # Without this converting opencv cannot save each frame in a video!
         frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        # frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img_rgb = frame
-        img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-        # x, y, h, w = 500, 440, 130, 110
+        # Convert to gray to better recognizing
+        img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Logic for "moving" window, in which opencv looks for recognition
+        # If have more than one recognized icon, increase window so it can handle next icon
         h = 50
         if number_of_frags != 0:
             h = 50 * (1 + number_of_frags)
         else:
             h = 50
+        # Set coordinates for "moving" recognition window
         x, y, h, w = 1000, 0, h, 200
-        cv2.rectangle(img_rgb, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        # Show "moving" window
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        # Crop recognition window from frame
         img_gray = img_gray[y:y+h, x:x+w]
+        # Load image, which will be trying to find
         template = cv2.imread('icon_player.PNG', 0)
         w, h = template.shape[::-1]
-        # 'cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
-        # 'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED'
+        # Opencv will try to find pixels, which are similar ot given icon
         res = cv2.matchTemplate(img_gray, template, cv2.TM_CCORR_NORMED)
+        # You may to play with this value a bit, cause results are sensitive to this value
         threshold = 0.93
+        # Remain only those pixels, which contain icon at given threshold
         loc = np.where(res >= threshold)
-        loc1 = loc[0]
-        loc1_prep = []
-        for i in range(len(loc[0])):
-            if i > 0:
-                if abs(loc[0][i]-prev[1]) > 10 or abs(loc[1][i]-prev[0]) > 10:
-                    prev = (loc[1][i], loc[0][i])
-                    loc1_prep.append(prev)
+        unique_icons = []
+        number_of_all_detected_icons = len(loc[0])
+        # Remain unique icon in a such way: consider unique if x and y coordinates at least 10 pixel different
+        for detected_icon_index in range(number_of_all_detected_icons):
+            x_of_detected_icon, y_of_detected_icon = loc[1][detected_icon_index], loc[0][detected_icon_index]
+            if detected_icon_index > 0:
+                if abs(x_of_detected_icon - previous_icon_x) > 10 or \
+                        abs(y_of_detected_icon - previous_icon_y) > 10:
+                    previous_icon_x, previous_icon_y = x_of_detected_icon, y_of_detected_icon
+                    unique_icons.append((previous_icon_x, previous_icon_y))
             else:
-                prev = (loc[1][i], loc[0][i])
-                loc1_prep.append(prev)
-
-        # print(f"Before: {list(zip(*loc[::-1]))}, after: {loc1_prep}")
-
-        num_of_detect = 0
-        for pt in loc1_prep:
-            num_of_detect += 1
-            cv2.rectangle(img_rgb, (pt[0] + x, pt[1] + y), (pt[0] + x + w, pt[1] + y + h), (0, 0, 255), 2)
-            # cv2.rectangle(img_rgb, (pt[0], pt[1]), (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-
-        frame = img_rgb
-        # gray = cv2.GaussianBlur(frame, (5, 5), 0)
-        # gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        # gray = cv2.GaussianBlur(gray, (5, 5), 0)
-        # frame = cv2.Canny(gray, 35, 125)
-        # frame = gray
+                previous_icon_x, previous_icon_y = x_of_detected_icon, y_of_detected_icon
+                unique_icons.append((previous_icon_x, previous_icon_y))
+        # Add coordinates of moving window to unique icon coordinates when make rectangle on frame
+        number_of_unique_icons = 0
+        for unique_icon in unique_icons:
+            number_of_unique_icons += 1
+            unique_icon_x, unique_icon_y = unique_icon[0], unique_icon[1]
+            cv2.rectangle(frame, (unique_icon_x + x, unique_icon_y + y),
+                          (unique_icon_x + x + w, unique_icon_y + y + h), (0, 0, 255), 2)
+        # Track seconds for screenplay
         how_much_seconds = int(time.time() - start_time)
-        if number_of_frags < num_of_detect:
-            number_of_frags = num_of_detect
+        # Increase frags if number of detected icons more than current number of frags else made equal
+        if number_of_frags < number_of_unique_icons:
+            number_of_frags = number_of_unique_icons
             total_frags += 1
-        if number_of_frags > num_of_detect:
-            number_of_frags = num_of_detect
-
+        if number_of_frags > number_of_unique_icons:
+            number_of_frags = number_of_unique_icons
+        # Add variables to screen for easy monitoring
         cv2.putText(frame, "FPS: %f" % (1.0 / (time.time() - last_time)),
                     (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.putText(frame,  f"Number of detection: {num_of_detect}", (30, 30),
+        cv2.putText(frame,  f"Number of detection: {number_of_unique_icons}", (30, 30),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         cv2.putText(frame,  f"Total frags: {total_frags}", (50, 50),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         cv2.putText(frame,  f"How much seconds: {how_much_seconds}", (70, 70),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-        # print("fps: {}".format(1 / (time.time() - last_time)))
+        # Save frame to video
         out.write(frame)
+        # Show frame to you
         cv2.imshow('frame', frame)
         # Press "q" to quit
         if cv2.waitKey(25) & 0xFF == ord("q"):
             break
-
-
 # Clean up
 out.release()
 cv2.destroyAllWindows()
